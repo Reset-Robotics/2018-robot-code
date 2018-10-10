44568416ec7776ace762000138373e0566170052
Unknown <PGD.daigle@gmail.com>
2018-10-10 18:18:50 -0400

    Added code in end and interrupted methods
    I was wondering why these commands weren't working as intended when using Button.whileHeld. Turns out they had no code to run on interrupted or end to set the motors back to 0.

--- src/org/usfirst/frc/team6325/robot/commands/Intake/SpinIntakeWheelsBack.java
+++ src/org/usfirst/frc/team6325/robot/commands/Intake/SpinIntakeWheelsBack.java
@@ -37,13 +37,13 @@ public class SpinIntakeWheelsBack extends Command {
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
-		
+		Robot.intake.setIntakePower(0);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
-		
+		end();
 	}
 }
