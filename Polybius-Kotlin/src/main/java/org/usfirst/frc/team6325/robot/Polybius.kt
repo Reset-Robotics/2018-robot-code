f9e8b0644c3e834901aa9d554d665af90a36e4df
Unknown <PGD.daigle@gmail.com>
2018-10-25 16:59:36 -0400

    Added basic Polybius.kt robot files, started on importing Drivetrain subsystem

---
+++ Polybius-Kotlin/src/main/java/org/usfirst/frc/team6325/robot/Polybius.kt
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team6325.robot
+
+import edu.wpi.first.wpilibj.CameraServer
+import org.usfirst.frc.team6325.robot.drivetrain.Drivetrain
+
+//import org.usfirst.frc.team6325.robot.util.UDPServer
+//import org.usfirst.frc.team6325.robot.util.initPreferences
+//import org.usfirst.frc.team6325.robot.util.logTelemetry
+import org.usfirst.frc.team6325.robot.Robot
+
+class Polybius : Robot() 
+{
+    override fun onCreate() 
+    {
+        Drivetrain
+
+
+        //UDPServer.start()
+        //CameraServer.getInstance().startAutomaticCapture()
+
+        //initPreferences()
+        //logTelemetry()
+    }
+}
