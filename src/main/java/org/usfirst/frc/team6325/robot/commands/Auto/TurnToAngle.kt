763c98458f9666d74ce5f3f157499108737861fe
Cyco12 <PGD.daigle@gmail.com>
2018-12-19 19:42:44 -0500

    Completed TurnToAngle.kt Command

--- src/main/java/org/usfirst/frc/team6325/robot/commands/Auto/TurnToAngle.kt
+++ src/main/java/org/usfirst/frc/team6325/robot/commands/Auto/TurnToAngle.kt
@@ -6,7 +6,7 @@ import org.sertain.command.Command
 
 public class TurnToAngle : Command
 {
-    var ang: Double = 0.0; var diff: Double = 0.0
+    var ang: Double = 0.0; var diff: Double = 0.0; var rate: = Double = 0.3
     var done: Boolean = false
 
     public fun TurnToAngle(angle: Double)
@@ -19,5 +19,32 @@ public class TurnToAngle : Command
     {
         diff = ang - Polybius.drivetrain.getAngle()
         
+        if(diff > 0)
+            Polybius.drivetrain.drive(rate, -rate)
+        else
+            Polybius.drivetrain.drive(-rate, rate)
+    }
+
+    override fun execute()
+    {
+        if (diff > 0)
+        {
+            if (Polybius.drivetrain.getAngle() > ang)
+                done = true
+        }
+        else
+        {
+            if (ang > Polybius.drivetrain.getAngle())
+                done = true
+        }
+    }
+    
+    override fun isCompleted(): Boolean = return done
+    override fun isCanceled() = onDestroy()
+
+    override fun onDestroy()
+    {
+        Polybius.drivetrain.autoTurnStop()
+        Polybius.drivetrain.killMotors()
     }
 }
